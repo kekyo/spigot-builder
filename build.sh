35ef1addaa36fe7d0c0ec1064c284569de37065d
Kouji Matsui <k@kekyo.net>
2022-06-22 13:03:36 +0900

    Added podman (docker) scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-sudo apt install openjdk-17-jdk
+podman build -t spigot_runner:1 .
 
-rm -rf stage
-mkdir stage
-cd stage
-
-curl --output BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-
-java -jar BuildTools.jar --rev 1.18.1
-
-cd ..
-cp stage/spigot-*.jar .
